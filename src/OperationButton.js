2b57b179bfef6baaafae34f3e9f828161c3c2f77
Naila <naile.gashimova@gmail.com>
2023-09-01 01:38:17 +0400

    edit

--- src/OperationButton.js
+++ src/OperationButton.js
@@ -1,13 +1,5 @@
-import { ACTIONS } from "./App"
+import {ACTIONS} from './App'
 
-export default function OperationButton({ dispatch, operation }) {
-  return (
-    <button
-      onClick={() =>
-        dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
-      }
-    >
-      {operation}
-    </button>
-  )
+export default function OperationButton ({dispatch, operation}) {
+    return  <button onClick={() => dispatch ({type: ACTIONS.CHOOSE_OPERATION, payload: {operation}})}>{operation}</button>
 }
